961ceee83305faff0bda4fff2978c065d36f449f
Igboanugwo Collins <munisco12@gmail.com>
2022-07-16 08:13:20 +0100

    working on styling

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,7 +1,9 @@
 import { NextResponse, NextRequest, NextFetchEvent } from 'next/server'
 export async function middleware(req: NextRequest, ev: NextFetchEvent) {
     const { pathname } = req.nextUrl
+    console.log('middleware')
     if (pathname == '/') {
+        
         return NextResponse.redirect('/comingsoon')
     }
     return NextResponse.next()
