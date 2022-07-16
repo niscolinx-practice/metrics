fa8a94be11a24c0f4f323b386fd8f0859d353cc4
Igboanugwo Collins <munisco12@gmail.com>
2022-07-16 07:33:50 +0100

    initial setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-blue-200">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt omnis placeat assumenda. Eveniet nam pariatur architecto? Repudiandae, officiis voluptatem. Possimus tempora non consequuntur atque corrupti, quis placeat fugiat et sed.
-    </div>
+      <div className='flex min-h-screen flex-col items-center justify-center py-2 bg-[#1c003e]'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          omnis placeat assumenda. Eveniet nam pariatur architecto? Repudiandae,
+          officiis voluptatem. Possimus tempora non consequuntur atque corrupti,
+          quis placeat fugiat et sed.
+      </div>
   )
 }
 
