bd13c56ccdfa33c6b111226e82faab6d9b28b667
Igboanugwo Collins <munisco12@gmail.com>
2022-07-16 09:33:56 +0100

    working on styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 //import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import '../sass/main.sass'
+import '../sass/main.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />
