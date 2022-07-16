f3c0ff5dfe9460fabbd58fda7bce24427342ff22
Igboanugwo Collins <munisco12@gmail.com>
2022-07-16 10:40:55 +0100

    working on styling

--- src/Containers/Layout.tsx
+++ src/Containers/Layout.tsx
@@ -1,12 +1,13 @@
-import React, { Children } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
-function Layout({children}) {
+const Layout: React.FC<{}> = ({children}) => {
   return (
+    <>
     <Header/>
     {children}
     <Footer/>
+    </>
   )
 }
 
