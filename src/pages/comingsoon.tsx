492326f1f10c056fbde7f57b5999b4fecf1f06a7
Igboanugwo Collins <munisco12@gmail.com>
2022-07-16 10:09:51 +0100

    working on styling

--- src/pages/comingsoon.tsx
+++ src/pages/comingsoon.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function comingSoon() {
   return (
-    <div className='comingSoon__layout'>
+    <div className='comingSoon'>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat aspernatur quas vitae quibusdam, perferendis velit sunt deleniti harum minima optio placeat nobis minus veniam, nam temporibus, praesentium enim molestias! Eius!
     </div>
   )
