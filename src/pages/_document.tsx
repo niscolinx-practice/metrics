da9563872a6019c2413d1ffa51ed282d94c680ec
Igboanugwo Collins <munisco12@gmail.com>
2022-07-16 12:14:21 +0100

    added sass and font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html>
-            <Head />
+            <Head>
+                <link rel='preconnect' href='https://fonts.googleapis.com' />
+                <link rel='preconnect' href='https://fonts.gstatic.com' />
+                <link
+                    href='https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,700;1,200&display=swap'
+                    rel='stylesheet'
+                />
+            </Head>
             <title>Metrics</title>
             <body>
                 <Main />
