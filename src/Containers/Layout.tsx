dfbd5f3c1cd22e188c1eb5288c9be06635880f64
Igboanugwo Collins <munisco12@gmail.com>
2022-07-16 10:45:48 +0100

    working on styling

--- src/Containers/Layout.tsx
+++ src/Containers/Layout.tsx
@@ -1,7 +1,12 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import { ReactNode } from 'react'
 
-const Layout: React.FC<{}> = ({children}) => {
+type LayoutProps = {
+    children: React.ReactNode
+}
+
+const Layout: React.FC<LayoutProps> = ({children}) => {
   return (
     <>
     <Header/>
