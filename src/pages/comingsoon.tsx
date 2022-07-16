4e92d6ae15c164f38026593ab3010c5b77f7b56f
Igboanugwo Collins <munisco12@gmail.com>
2022-07-16 13:12:41 +0100

    added sass and font

--- src/pages/comingsoon.tsx
+++ src/pages/comingsoon.tsx
@@ -3,6 +3,9 @@ import React from 'react'
 function comingSoon() {
   return (
     <div className='comingSoon'>
+      <div className="ellipse">
+
+      </div>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat aspernatur quas vitae quibusdam, perferendis velit sunt deleniti harum minima optio placeat nobis minus veniam, nam temporibus, praesentium enim molestias! Eius!
     </div>
   )
