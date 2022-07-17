cf4ee933b816e1ec2be74403ad74e7a3c67f4723
Igboanugwo Collins <munisco12@gmail.com>
2022-07-17 23:49:44 +0100

    working on coming soon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,48 +2,54 @@ import Link from 'next/link'
 import React from 'react'
 import { BsYoutube } from 'react-icons/bs'
 import { AiFillFacebook, AiFillLinkedin, AiFillInstagram } from 'react-icons/ai'
-import {FaTwitterSquare} from 'react-icons/fa'
+import { FaTwitterSquare } from 'react-icons/fa'
 
 function Footer() {
-  return (
-      <footer className='footer'>
-          <ul className='footer__links'>
-              <li className='footer__links--item'>
-                  <Link href='/'>
-                      <BsYoutube />
-                  </Link>
-              </li>
-              <li className='footer__links--item'>
-                  <Link href='/'>
-                      <AiFillFacebook />
-                  </Link>
-              </li>
-              <li className='footer__links--item'>
-                  <Link href='/'>
-                      <AiFillLinkedin />
-                  </Link>
-              </li>
-              <li className='footer__links--item'>
-                  <Link href='/'>
-                      <AiFillInstagram />
-                  </Link>
-              </li>
-              <li className='footer__links--item'>
-                  <Link href='/'>
-                      <FaTwitterSquare />
-                  </Link>
-              </li>
-          </ul>
+    return (
+        <footer className='footer'>
+            <ul className='footer__links'>
+                <li className='footer__links--item'>
+                    <Link href='/'>
+                        <BsYoutube />
+                    </Link>
+                </li>
+                <li className='footer__links--item'>
+                    <Link href='/'>
+                        <AiFillFacebook />
+                    </Link>
+                </li>
+                <li className='footer__links--item'>
+                    <Link href='/'>
+                        <AiFillLinkedin />
+                    </Link>
+                </li>
+                <li className='footer__links--item'>
+                    <Link href='/'>
+                        <AiFillInstagram />
+                    </Link>
+                </li>
+                <li className='footer__links--item'>
+                    <Link href='/'>
+                        <FaTwitterSquare />
+                    </Link>
+                </li>
+            </ul>
 
-          <div className='footer__contracts'>
-              <Link href='/' className='footer__contracts--item'>Terms of services</Link>
-              <Link href='/' className='footer__contracts--item'>Privacy Policy</Link>
-          </div>
-          <div className='footer__copyright'>
-              <p>Copyright 2021 @ Peddle Technologies. All Rights Reserved.</p>
-          </div>
-      </footer>
-  )
+            <div className='footer__contracts'>
+                <Link href='/' className='footer__contracts--item'>
+                    Terms of services
+                </Link>
+                <Link href='/' className='footer__contracts--item'>
+                    Privacy Policy
+                </Link>
+            </div>
+            <div className='footer__copyright'>
+                <p>
+                    Copyright 2021 @ Peddle Technologies. All Rights Reserved.
+                </p>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer
