dcd9bf904cc116edfed5a8893b43053f1b661f41
Igboanugwo Collins <munisco12@gmail.com>
2022-07-17 23:20:00 +0100

    working on coming soon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link'
 import React from 'react'
-import {BsYoutube} from 'react-icons/bs'
+import { BsYoutube, BsLinkedin } from 'react-icons/bs'
 import {AiFillFacebook} from'react-icons/ai'
+import {RiInstagramLine} from'react-icons/ri'
+import {FaTwitterSquare} from 'react-icons/fa'
 
 function Footer() {
   return (
