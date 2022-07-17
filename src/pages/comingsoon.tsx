f75b6fe927a4de5f96d26b30acd2a88fba6443cb
Igboanugwo Collins <munisco12@gmail.com>
2022-07-17 22:07:58 +0100

    added fonts

--- src/pages/comingsoon.tsx
+++ src/pages/comingsoon.tsx
@@ -12,29 +12,29 @@ function comingSoon() {
                 <div className='comingSoon__ellipse--3'></div>
             </div>
 
-            <div className='comingSoon__content'>
-                <h1 className='comingSoon__content--header'>
+            <div className='comingSoon__rapper'>
+                <h1 className='comingSoon__header'>
                     Something awesome is coming soon
                 </h1>
-                <p className='comingSoon__content--text'>
+                <p className='comingSoon__text'>
                     Your all-in-one affiliate marketing tracking software{' '}
                     <span>track</span>, <span>automate</span> and{' '}
                     <span>optimize</span> your campaigns.
                 </p>
-                <div className='comingSoon__content--timer'>
-                    <div className='comingSoon__content--timer-item'>
+                <div className='comingSoon__timer'>
+                    <div className='comingSoon__timer--item'>
                         <span className='span-1'>7</span>
                         <span className='span-2'>Days</span>
                     </div>
-                    <div className='comingSoon__content--timer-item'>
+                    <div className='comingSoon__timer--item'>
                         <span className='span-1'>24</span>
                         <span className='span-2'>Hours</span>
                     </div>
-                    <div className='comingSoon__content--timer-item'>
+                    <div className='comingSoon__timer--item'>
                         <span className='span-1'>54</span>
                         <span className='span-2'>Minutes</span>
                     </div>
-                    <div className='comingSoon__content--timer-item'>
+                    <div className='comingSoon__timer--item'>
                         <span className='span-1'>11</span>
                         <span className='span-2'>Second</span>
                     </div>
