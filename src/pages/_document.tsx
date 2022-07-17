9e6cf1422b41f4d30515b99769494ae63b75d924
Igboanugwo Collins <munisco12@gmail.com>
2022-07-17 21:23:44 +0100

    added blobs

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,13 +10,7 @@ export default function Document() {
                     href='https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,700;1,200&display=swap'
                     rel='stylesheet'
                 />
-                <link
-                    rel='preload'
-                    href='/fonts/AdobeCleanBlack.otf'
-                    as='font'
-                    type='font/otf'
-                    crossOrigin=''
-                />
+               
             </Head>
             <title>Metrics</title>
             <body>
