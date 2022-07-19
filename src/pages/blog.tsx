bb790f052f624c6563058e844bb2deae09b5473a
Igboanugwo Collins <munisco12@gmail.com>
2022-07-19 13:53:27 +0100

    working on blog

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -6,14 +6,11 @@ function blog() {
             <div className='blog__banner'>
                 <div className='blog__banner--left'></div>
                 <div className='blog__banner--text'>
-                        <p className='about__header--2__subTitle'>
-                            <span className='span-1'></span>
-                            <span className='span-2'>ABOUT US</span>
-                        </p>
-                        <p className='about__header--2__title'>
-                            <span>Built for SaaS</span>{' '}
-                            <span>and E-commerce</span>
-                        </p>
+                    <p className='text__subTitle'>
+                        <span className='span-1'></span>
+                        <span className='span-2'>blog</span>
+                    </p>
+                    <p className='text__title'>Articles and News</p>
                 </div>
                 <div className='blog__banner--left-sub'></div>
                 <div className='blog__banner--right'></div>
