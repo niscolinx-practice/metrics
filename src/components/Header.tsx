097e3efe538d4f6d8aa75dd822fc1c5de9700b37
Igboanugwo Collins <munisco12@gmail.com>
2022-07-19 12:46:44 +0100

    added router nav change for blog

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,29 @@
 import Link from 'next/link'
 import React from 'react'
 
-const Header = ({isComingSoon, isBlog}: {isComingSoon: boolean, isBlog: boolean}) => {
 
-    const useStyle:React.CSSProperties = {
-        display: isComingSoon ? 'none' : 'flex'
-        
+
+const Header = ({
+    isComingSoon,
+    isBlog,
+}: {
+    isComingSoon: boolean
+    isBlog: boolean
+}) => {
+    const useStyle: React.CSSProperties = {
+        display: isComingSoon ? 'none' : 'flex',
+        background: isBlog ? 'white' : '',
     }
 
     return (
-        <header
-            className='header'
-            style={useStyle}
-        >
+        <header className='header' style={useStyle}>
             <div className='header__logo'>
                 <Link href='/'>
                     <img src='logo-white.png' alt='' />
                 </Link>
             </div>
 
-            <ul className='header__lists'>
+            <ul className='header__lists' style={{ useLinkStyles }}>
                 <li className='header__lists--item'>
                     <Link href='/about'> About Us</Link>
                 </li>
