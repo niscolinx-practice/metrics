bffdea82981ffd9438c2c6bcc488e35ddb8aa4a8
Igboanugwo Collins <munisco12@gmail.com>
2022-07-19 13:51:54 +0100

    working on blog

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
 function blog() {
-  return (
-    <div className='blog'>
-
-    </div>
-  )
+    return (
+        <div className='blog'>
+            <div className='blog__banner'>
+                <div className='blog__banner--left'></div>
+                <div className='blog__banner--text'>
+                        <p className='about__header--2__subTitle'>
+                            <span className='span-1'></span>
+                            <span className='span-2'>ABOUT US</span>
+                        </p>
+                        <p className='about__header--2__title'>
+                            <span>Built for SaaS</span>{' '}
+                            <span>and E-commerce</span>
+                        </p>
+                </div>
+                <div className='blog__banner--left-sub'></div>
+                <div className='blog__banner--right'></div>
+            </div>
+        </div>
+    )
 }
 
 export default blog
