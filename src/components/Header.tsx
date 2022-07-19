42ec0508988552dc1d362e226afca9a9a1270c25
Igboanugwo Collins <munisco12@gmail.com>
2022-07-19 12:40:27 +0100

    added router nav change for blog

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
 
-const Header = ({isComingSoon}: {isComingSoon: boolean}) => {
+const Header = ({isComingSoon, isBlog}: {isComingSoon: boolean, isBlog: boolean}) => {
     return (
         <header
             className='header'
